5bb872d4d5610ac11073600d1a322dd1823c0fcc
Andrew <andrewliao11@gmail.com>
2021-04-25 11:02:52 -0400

    add demo template

---
+++ demo/js/scripts.js
@@ -0,0 +1,29 @@
+// Empty JS for your own code to be here
+$('#run-btn').on('click', function (e) {
+    var n_workers = $('#select-n_workers').val();
+    var n_gold_q = $('#select-n_gold_q').val();
+    var agg = $('#select-agg').val();
+    var features = $('#select-features').val();
+    var model_freq = $('#select-model_freq').val();
+    var use_ta = $('#checkbox-ta').is(":checked");
+    var use_early_stop = $('#checkbox-early_stop').is(":checked");
+
+    console.log(use_ta);
+    console.log(use_early_stop);
+    console.log(agg);
+    console.log(n_gold_q);
+    console.log(model_freq);
+    console.log(features);
+
+    if (n_workers.length == 0 || n_gold_q.length == 0 || agg.length == 0){
+        msg = 'Some items are empty'
+        window.alert(msg);
+    }
+    else if ((agg == 'Ours: w/ Semi-supervised CV model' || agg == 'Prior work: w/ CV model') && (features.length == 0 || model_freq.length == 0)) {
+        msg = 'CV mode is used. Please select the features and the model update frequency'
+        window.alert(msg);
+    }
+    else {
+        window.alert('Run');
+    }
+})
